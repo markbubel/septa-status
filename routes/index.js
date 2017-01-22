92b4f03a5085e9f3e67a6f5ccd8a43071b858204
Mark <mark.bubel@gmail.com>
2017-01-22 17:20:22 -0500

    adding About page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'SEPTA Service Status' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'SEPTA Service Status' });
+});
+
 module.exports = router;
