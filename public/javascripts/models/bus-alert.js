0cf95ff3870880bddf99dce75b5669510f799104
Mark <mark.bubel@gmail.com>
2017-01-27 18:50:07 -0500

    updates

--- public/javascripts/models/bus-alert.js
+++ public/javascripts/models/bus-alert.js
@@ -1,5 +1,12 @@
 class BusAlert extends Alert {
+    addClickListener() {
+        document.getElementsByClassName("btn").addEventListener('click', function() {
+            console.log("clicked on: ");
+        }, false);
+    }
+
     render() {
+        this.addClickListener();
         return `
       <div class="childOfbus-alerts-container ${this.isGoodService() ? 'bg-success' : 'bg-danger'}">
         ${this.route_name}
