4761bf27bf6b60e65670295a399ea7215b6e08b6
Mark <mark.bubel@gmail.com>
2017-01-28 09:24:14 -0500

    added bus search

--- public/javascripts/models/rr-alert.js
+++ public/javascripts/models/rr-alert.js
@@ -1,14 +1,10 @@
 class RegionalRailAlert extends Alert {
     render() {
         return `
-            <div class="panel ${this.isGoodService() ? 'panel-success' : 'panel-danger'}">
-                <div class="panel-heading">
-                    <h3 class="panel-title">${this.route_name}</h3>
-                </div>
-                <div class="panel-body">
-                    ${this.isGoodService() ? 'Good service' : this.renderMessage(this.current_message, false)}
-                </div>
-            </div>
+            <li class="list-group-item ${this.isGoodService() ? 'list-group-item-success' : 'list-group-item-danger'}">
+                ${this.route_name}
+                <p class="alertMessage">${this.isGoodService() ? 'Good service' : this.renderMessage(this.current_message, false)}</p>
+            </li>
         `;
     }
 }
