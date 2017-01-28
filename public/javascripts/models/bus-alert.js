86b9c7b5505caec529369e346cdaa138da8e7814
Mark <mark.bubel@gmail.com>
2017-01-27 20:51:33 -0500

    updates

--- public/javascripts/models/bus-alert.js
+++ public/javascripts/models/bus-alert.js
@@ -1,20 +1,18 @@
 class BusAlert extends Alert {
     constructor() {
         super();
-        this.element = document.createElement('div');      
-        this.element.className = `childOfbus-alerts-container ${this.isGoodService() ? 'bg-success' : 'bg-danger'}`;
-        this.element.textContent = this.route_name;
-        this.element.addEventListener('click', this.getDetails);
-        console.log(this.route_name);
-          
+        this.element = document.createElement('div');
+        this.element.addEventListener('click', this.getDetails.bind(this));
 }
 
 getDetails() {
    var details = document.getElementById('bus-alert-details');
-   details.innerHTML = this.current_message;
+   details.innerHTML = this.advisory_message;
 }
 
 render() {
+    this.element.className = `childOfbus-alerts-container ${this.isGoodService() ? 'bg-success' : 'bg-danger'}`;
+    this.element.textContent = this.route_name;
     return this.element;
 }
 }
