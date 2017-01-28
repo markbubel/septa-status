a41bf7da0f844751448e98f7b07b37ddc68d33e3
Mark <mark.bubel@gmail.com>
2017-01-27 22:40:02 -0500

    updates

--- public/javascripts/models/bus-alert-list.js
+++ public/javascripts/models/bus-alert-list.js
@@ -9,10 +9,9 @@ class BusAlertList {
 
     searchRoute(theQuery) {
         for (var i = 0; i < this.alerts.length; i++) {
-            if (theQuery !== this.alerts[i].route_name) {
-                console.log("not a route");
+            if (theQuery === this.alerts[i].route_name) {
+                break;
             }
-            if (theQuery === this.alerts[i].route_name) break;
   }
 
         document.getElementById("bus-alert-details").innerHTML = this.alerts[i].advisory_message;
