90b9f22fd41dcbb4e6746f9f7f5bd422c742f136
Mark <mark.bubel@gmail.com>
2017-08-28 19:49:49 -0400

    starting over in pure JS

--- public/javascripts/models/app.js
+++
@@ -1,6 +0,0 @@
-class App {
-    constructor() {
-        if(window.location.pathname.includes('regional-rail')) this.alertList = new RegionalRailAlertList();
-        else if(window.location.pathname.includes('buses')) this.alertList = new BusAlertList();
-    }
-}
