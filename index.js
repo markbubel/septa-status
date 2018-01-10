092dc81192c20d99b2b86bf2748a3cc874de473d
Mark <mark.bubel@gmail.com>
2018-01-10 16:29:26 -0500

    Updates

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-// set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
